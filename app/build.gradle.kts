e897a0f22398333a0477b60c18e01583a0fefeee
hktnaydn <haktanbt@gmail.com>
2026-07-01 08:17:33 +0300

    Bump release version to 0.1.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.hktnv.iptvbox"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 12
-        versionName = "0.1.11"
+        versionCode = 13
+        versionName = "0.1.12"
     }
 
     signingConfigs {
